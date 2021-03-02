e180171505c797c1b0ca854894c7a14437822112
nguyenanhvan29062000 <nguyenanhvan.29062000@gmail.com>
2021-03-02 20:44:35 +0700

    1.0.2

--- Genshin_impact.sh
+++ Genshin_impact.sh
@@ -4,3 +4,4 @@ bash $CACHE/gilinux/132/patch.sh
 bash $CACHE/gilinux/132/patch_anti_logincrash.sh
 echo "PRESS KEY TO FINISH"
 read key
+env __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia DXVK_HUD=1 WINEARCH=win64 WINEPREFIX=$GAMEDIR $HOME/.local/share/lutris/runners/wine/lutris-6.0-x86_64/bin/wine cmd /c launcher.bat
