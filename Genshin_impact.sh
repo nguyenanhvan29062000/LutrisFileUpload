0741e6c5afbb343d0ef37597c16cf36158ae0b14
nguyenanhvan29062000 <nguyenanhvan.29062000@gmail.com>
2021-03-02 18:03:49 +0700

    1.0.1

--- Genshin_impact.sh
+++ Genshin_impact.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/bash env
 cd $GAMEDIR/drive_c/Program\ Files/Genshin\ Impact/Genshin\ Impact\ game/
 bash $CACHE/gilinux/132/patch.sh
 bash $CACHE/gilinux/132/patch_anti_logincrash.sh
+echo "PRESS KEY TO FINISH"
+read key
